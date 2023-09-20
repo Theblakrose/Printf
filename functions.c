dbbb8bc292225d249424cc997bfef80b7b4441a6
Theblakrose <Petronellakhumalov@gmail.com>
2023-09-20 23:23:33 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -8,6 +8,7 @@
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
+ *Petronella
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
